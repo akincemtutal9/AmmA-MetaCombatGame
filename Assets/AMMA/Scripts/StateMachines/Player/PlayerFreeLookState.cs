d543690d3f6e9108f01a7e053f6b3fbf27e64907
akincemtutal9 <akintutall@gmail.com>
2022-08-03 16:14:25 +0300

    State Driven Camera
    State Driven Camera

--- Assets/AMMA/Scripts/StateMachines/Player/PlayerFreeLookState.cs
+++ Assets/AMMA/Scripts/StateMachines/Player/PlayerFreeLookState.cs
@@ -6,12 +6,16 @@ public class PlayerFreeLookState : PlayerBaseState
 {
     private readonly int FreeLookSpeedHash = Animator.StringToHash("FreeLookSpeed");
 
+    private readonly int FreeLookBlendTreeHash = Animator.StringToHash("FreeLookBlendTree");
+
+
     private const float AnimatorDampTime = 0.1f;
     public PlayerFreeLookState(PlayerStateMachine stateMachine) : base(stateMachine) { }
 
     public override void Enter()
     {
-        stateMachine.InputReader.TargetEvent += OnTarget;    
+        stateMachine.InputReader.TargetEvent += OnTarget;
+        stateMachine.Animator.Play(FreeLookBlendTreeHash);
     }
     public override void Tick(float deltaTime)
     {
--- Assets/AMMA/Scripts/StateMachines/Player/PlayerTargetingState.cs
+++ Assets/AMMA/Scripts/StateMachines/Player/PlayerTargetingState.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class PlayerTargetingState : PlayerBaseState
 {
+
+    private readonly int TargetingBlendTreeHash = Animator.StringToHash("TargetingBlendTree");
+
     public PlayerTargetingState(PlayerStateMachine stateMachine) : base(stateMachine) { }
 
     public override void Enter()
     {
         stateMachine.InputReader.CancelEvent += OnCancel;
+        stateMachine.Animator.Play(TargetingBlendTreeHash);
     }
 
     public override void Tick(float deltaTime)
